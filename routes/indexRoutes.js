ef449b3a24fbd7cbb0a31a72d1664ae7ac1bf18b
deepakchandra4 <deepakchandra4551@gmail.com>
2025-02-17 18:47:20 +0530

    Post Jobs Sections Added

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -8,6 +8,11 @@ router.get('/', (req, res) => {
     res.render('index', { user: req.user });
 });
 
+//post jobs page
+router.get('/post-jobs', (req, res) => {
+    res.render('postJobs', { user: req.user });
+}); 
+
 // Dashboard Route (Protected)
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
     res.render('dashboard', { user: req.user });
